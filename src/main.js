4afd1b2d1cd2bf054835fc7ab2ba11499b738ec8
TimmonsN <nikotimmons9@gmail.com>
2026-04-15 22:08:11 -0400

    compass

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 function main() {
     console.log("Hello World");
 }
+function init(){
+  console.log("it worked");
+  var orientation = document.getElementById("compass");
+  
+  if (window.DeviceOrientationEvent){
+    window.addEventListener('deviceorientation', function(event){
+      var a = event.alpha;
+      var b = event.beta;
+      var g = event.gamma;
 
+      orientation.innerhtml="a= " + a + "b= " + b + "g= " + g;
+    },false)
+  }
+}
 main();
