608e7171889fb3684adfd9a2e9fcb227c70eac47
TimmonsN <nikotimmons9@gmail.com>
2026-04-19 16:16:03 -0400

    picked location 2 test 2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const arrow = document.getElementById("arrow");
 let orientation = document.getElementById("compass");
 // let origin = {x : 0, y : 0};
 
-let origin = {x:39.964868, y:-83.005217};
+let origin = {x:39.964679, y:-83.005196};
 
 let currentPos = {x : 0, y : 0};
 let theta = 0;
